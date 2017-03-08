d4502114c15ea83affc1e5a00cc37eb5834f881b
musicmunky <musicmunky@gmail.com>
2017-03-08 07:23:00 -0500

    Update with additional pages and controllers, with partials for head tag and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
 	get 'pages/index'
 	root 'pages#index'
 
+	get 'about', to: 'about#index', as: :about
+	get 'about/index'
+
+	get 'contact', to: 'contact#index', as: :contact
+	get 'contact/index'
+
+	get 'resources', to: 'resources#index', as: :resources
+	get 'resources/index'
+
 #	get 'pages/oops'
 #	get 'oops' => 'pages#oops'
 
