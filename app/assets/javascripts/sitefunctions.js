e2d0ff6fea1db4080803d54fdde768dbb6e00a25
Tim Andrews <musicmunky@gmail.com>
2017-05-05 00:34:22 +0000

    update with sticky header and other style changes along with embedded twitter timeline

--- app/assets/javascripts/sitefunctions.js
+++ app/assets/javascripts/sitefunctions.js
@@ -1,3 +1,20 @@
 
 $( document ).ready(function() {
+
+    $(window).scroll(function () {
+        console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 170) {
+            $('#navbar').addClass('navbar-fixed');
+            $('#navbar-inner').addClass('navbar-inner-fixed');
+            $('.title').css('height', "200px");
+            $('.titlelinks').css('border', "none");
+        }
+        if ($(window).scrollTop() < 171) {
+            $('#navbar').removeClass('navbar-fixed');
+            $('#navbar-inner').removeClass('navbar-inner-fixed');
+            $('.title').css('height', "150px");
+            $('.titlelinks').css('border-top', "1px solid #c1b8be");
+        }
+    });
+
 });
