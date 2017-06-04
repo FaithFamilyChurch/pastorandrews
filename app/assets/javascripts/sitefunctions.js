c2387cf42564edd3b27541f3c3cbd4676589281b
Tim Andrews <musicmunky@gmail.com>
2017-06-04 16:33:12 +0000

    Update with nav bar indicator fixes from Randy

--- app/assets/javascripts/sitefunctions.js
+++ app/assets/javascripts/sitefunctions.js
@@ -18,12 +18,16 @@ jQuery( document ).on('turbolinks:load', function() {
             $('#navbar-inner').addClass('navbar-inner-fixed');
             $('.title').css('height', "200px");
             $('.titlelinks').css('border', "none");
+            $('.nav-indicator').css('margin-top', '0px');
+            $('.titlelink').css('padding', "4px 10px 10px 10px");
         }
         if ($(window).scrollTop() < 171) {
             $('#navbar').removeClass('navbar-fixed');
             $('#navbar-inner').removeClass('navbar-inner-fixed');
             $('.title').css('height', "150px");
             $('.titlelinks').css('border-top', "1px solid #c1b8be");
+            $('.nav-indicator').css('margin-top', '-6px');
+            $('.titlelink').css('padding', "10px");
         }
     });
 
