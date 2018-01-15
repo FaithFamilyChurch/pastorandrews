fb1d6dd25f5e3770678197e8fe9a97eae56cabf7
Tim Andrews <musicmunky@gmail.com>
2018-01-15 13:46:28 +0000

    Update to fix home page along with about page, and updates to gemfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+
+    def execute_statement(sql)
+        results = ActiveRecord::Base.connection.execute(sql)
+        if results.present?
+            return results
+        else
+            return nil
+        end
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,62 @@
 class PagesController < ApplicationController
 
+    require 'nokogiri'
+
 	def index
+        @aBlogList = execute_statement("select * from pastorblog_dev.contents order by published_at desc limit 4;")
+        @aArticles = []
+
+        @sDomain = "http://ffcpastor.com/"
+
+        @bShowRecent = false
+
+        sRecentTime = @aBlogList.first[18].to_s
+        oRecent = Time.parse(sRecentTime)
+        oCrrnt = Time.new
+        oTDiff = (((oCrrnt - oRecent) / 24) / 3600)
+        nTDiff = '%.0f' % oTDiff
+
+        if oTDiff < 5
+            @bShowRecent = true
+        end
+
+        for aBlog in @aBlogList do
+            @aTmp = {}
+            oContent = Nokogiri::HTML.fragment(aBlog[4])
+            oPTag = oContent.search('p')
+            oImg = oContent.search('img')
+            sImg = ""
+            sTxt = ""
+
+            if oPTag.first.search('img').size > 0
+                sTxt = oPTag[1].inner_html
+            else
+                sTxt = oPTag.first.inner_html
+            end
+
+            aTxt = sTxt.split(".")
+            aRgx = aTxt[0].split(/\.|\?|\!/)
+            sTxt = "#{aRgx[0]}."
+
+            if oImg.size > 0
+                eImg = oImg.first
+                sImg = eImg['src']
+            end
+
+            oDate = Time.parse(aBlog[18].to_s)
+            sMonth = '%02d' % oDate.month
+            sDay = '%02d' % oDate.day
+            sLink = "blog/#{oDate.year}/#{sMonth}/#{sDay}/#{aBlog[10]}"
+
+            @aTmp['link']  = sLink
+            @aTmp['title'] = aBlog[2]
+            @aTmp['date']  = aBlog[18].strftime("%^b %-d, %Y")
+            @aTmp['image'] = sImg
+            @aTmp['text']  = sTxt
+
+            @aArticles.push @aTmp
+        end
+
 	end
 
     def requestNewSubscription
